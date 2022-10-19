fdeb191493b9486980b017cb4b2f27b710716a42
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-10-19 18:05:43 +0000

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './style.css';
+import './modules/commentPopup.css';
 import { movieList } from './modules/displayPage.js';
+import { module } from './../node_modules/@webassemblyjs/ast/esm/nodes';
 
 document.addEventListener('DOMContentLoaded', () => {
   movieList();
